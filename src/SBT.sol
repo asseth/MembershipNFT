201fb64bf6f40f036a52f6303a99d693c837436e
jhache <julien.hache7@gmail.com>
2023-05-23 16:23:43 +0200

    feat: Add basic mint & burn function for testing purpose.

--- src/SBT.sol
+++ src/SBT.sol
@@ -3,7 +3,32 @@
 pragma solidity ^0.8.13;
 
 import {ERC5192} from "ERC5192/ERC5192.sol";
+import {Counters} from "openzeppelin-contracts/contracts/utils/Counters.sol";
 
 contract SBT is ERC5192 {
+    using Counters for Counters.Counter;
+
+    /// @dev The current supply (number of token minted) is handled with a Counter.
+    /// See https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol
+    Counters.Counter private _tokenSupply;
+
     constructor(string memory name_, string memory symbol_) ERC5192(name_, symbol_, true) {}
+
+    /// @dev Returns the total amount of tokens stored by the contract.
+    function totalSupply() public view returns (uint256) {
+        return _tokenSupply.current();
+    }
+
+    function mint() public {
+        _tokenSupply.increment();
+        _mint(msg.sender, _tokenSupply.current());
+        emit Locked(_tokenSupply.current());
+    }
+
+    function burn(uint256 tokenId) public {
+        // NB: No operator allowed when contract is locked
+        //     So no need to check them
+        require(ownerOf(tokenId) == msg.sender);
+        _burn(tokenId);
+    }
 }
